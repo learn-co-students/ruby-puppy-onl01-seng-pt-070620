7a07b870cfaab745f7350c098700cf4082559c01
Jill Short <jillian.b.short@gmail.com>
2020-07-27 20:55:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+   @@all
+  end
+  
+  def self.clear_all
+    @@all.clear 
+    puts "#{@@all}"
+  end
+  
+  def self.print_all
+    @@all.each do |id, @name|
+      
+    end
+  end
+  
+end
