06ee96106e724a3a460ce3159d5320c8e2373a16
Denise Henderson <denise@henderson3.us>
2020-07-28 02:27:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+require 'pry'
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    print_list = []
+    print_list = @@all.map(&:name).join("\n")
+    print print_list + "\n"
+  end
+
+  def self.save
+    @@all << self.name
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
