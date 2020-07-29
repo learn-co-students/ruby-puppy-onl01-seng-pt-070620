5a859d3c3e3729a7c5f7a0761e409d56aee41130
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-29 00:18:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog 
+  
+  @@all = [] #Class variable that is set to empty Array
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.print_all
+    @@all.each do |pup|
+      puts pup.name 
+    end
+  end
+  
+  def save
+    @@all << self 
+  end
+  
+  def self.clear_all
+    @@all.clear 
+  end 
+end   # End Class 
